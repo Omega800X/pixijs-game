9b2ba7045e8a93df8c2c9c9dc69e2fc15ccc875a
Fernando David Medvedovsky <fernandodavidmedve56@gmail.com>
2024-04-18 16:01:29 -0300

    Assets file

---
+++ src/assets.ts
@@ -0,0 +1,23 @@
+import { Assets, AssetsManifest } from "pixi.js";
+
+export const manifest : AssetsManifest = {
+    bundles: [
+        {
+            name: "assets",
+            assets:
+            {
+                "Player" : "./Player.png",
+                "Enemy" : "./EnemyBall.png"
+            }
+        },
+    ]
+}
+
+export async function initialize_assets() {
+	// Assets.init must only happen once!
+	// Pack all your bundles into one manifest!
+	await Assets.init({ manifest: manifest });
+
+	// Load the bundles you need
+	await Assets.loadBundle("assets");
+}
