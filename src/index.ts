9d2c16a5d06652a3fb9a069df0e68e7f95ed9d90
Fernando David Medvedovsky <fernandodavidmedve56@gmail.com>
2024-04-18 16:10:38 -0300

    Updated index

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
-import { Application, Sprite } from 'pixi.js'
-import { BACKGROUND_COLOR, SCREEN_HEIGHT, SCREEN_WIDTH } from './utils/constants';
+import { TitleScreenScene } from "./scenes/TitleScreenScene";
+import { SceneManager } from "./utils/SceneManager";
 
-const app = new Application<HTMLCanvasElement>({
-	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
-	resolution: window.devicePixelRatio || 1,
-	autoDensity: true,
-	backgroundColor: BACKGROUND_COLOR,
-	width: SCREEN_WIDTH,
-	height: SCREEN_HEIGHT
-});
-
-const clampy: Sprite = Sprite.from("clampy.png");
-
-clampy.anchor.set(0.5);
-
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
-
-app.stage.addChild(clampy);
+SceneManager.initialize();
+SceneManager.changeScene(new TitleScreenScene());
