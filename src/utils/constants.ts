2556b3ae173bc184ab481f3de6bbd678e977b3d8
Fernando David Medvedovsky <fernandodavidmedve56@gmail.com>
2024-04-19 14:22:46 -0300

    Added font name and game title to constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
+export const GAME_NAME = "Dodge'em all!"
 export const SCREEN_WIDTH = 1920;
 export const SCREEN_HEIGHT = 1080;
 export const BACKGROUND_COLOR = 0xfdfff7;
+export const FONT = "AbstractGroovy";
