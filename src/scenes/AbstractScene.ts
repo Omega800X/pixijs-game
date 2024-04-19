016288380b1d7f66dd8b1c0b01be1caac6667c8f
Fernando David Medvedovsky <fernandodavidmedve56@gmail.com>
2024-04-19 14:22:00 -0300

    Changed update method from AbstractScene

--- src/scenes/AbstractScene.ts
+++ src/scenes/AbstractScene.ts
@@ -2,5 +2,5 @@ import { Container } from "pixi.js";
 
 export abstract class AbstractScene extends Container {
     
-    public abstract update(deltaFrame : number, deltaTime? : number) : void;
+    public abstract update(deltaTime : number) : void;
 }
