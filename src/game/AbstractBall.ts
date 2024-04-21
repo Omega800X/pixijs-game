389b8dbc8d90b716b17bab5535ff5ee19336eb24
Fernando David Medvedovsky <fernandodavidmedve56@gmail.com>
2024-04-21 16:18:08 -0300

    change abstract ball to put any radius

--- src/game/AbstractBall.ts
+++ src/game/AbstractBall.ts
@@ -3,23 +3,24 @@ import { Container, Point, Sprite } from "pixi.js";
 export abstract class AbstractBall extends Container {
 
     protected sprite : Sprite;
-    protected readonly radius : number = 28;
+    protected readonly radius : number;
     protected speed : Point = new Point();
     protected velocity : number;
     
     
 
-    constructor(sprite : string, velocity : number) {
+    constructor(sprite : string, velocity : number, radius : number) {
         super();
 
         this.sprite = Sprite.from(sprite);
         this.velocity = velocity;
+        this.radius = radius;
 
         this.sprite.anchor.set(0.5);
         this.addChild(this.sprite);
     }
 
-    public is_colliding(Ball : AbstractBall) : boolean {
+    public isColliding(Ball : AbstractBall) : boolean {
         
         const dx = Ball.x - this.x;
         const dy = Ball.y - this.y;
